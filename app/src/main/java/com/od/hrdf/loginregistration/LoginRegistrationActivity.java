69c3aabd2fa10eae7f5137aa7db4f1dd9212d052
MuhammadMahmoor <mahmoor@opendynamics.com.my>
2016-10-20 17:19:11 +0800

    login and registeration feature added

--- app/src/main/java/com/od/hrdf/loginregistration/LoginRegistrationActivity.java
+++ app/src/main/java/com/od/hrdf/loginregistration/LoginRegistrationActivity.java
@@ -1,15 +1,101 @@
 package com.od.hrdf.loginregistration;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
+import com.od.hrdf.BOs.User;
 import com.od.hrdf.R;
+import com.od.hrdf.Utils.Util;
 
-public class LoginRegistrationActivity extends AppCompatActivity {
+import io.realm.Realm;
+
+public class LoginRegistrationActivity extends AppCompatActivity implements LoginRegActivityInterface {
+
+    FragmentManager fragmentManager;
+    private Toolbar toolbar;
+    private Realm realm;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_registration);
+        realm = Realm.getDefaultInstance();
+        user = User.getCurrentUser(realm);
+        if (user != null) {
+            Toast.makeText(this, "Ready to go to landing", Toast.LENGTH_SHORT).show();
+        } else {
+            initViews();
+            fragmentManager = getSupportFragmentManager();
+            addFragment(LoginFragment.newInstance("", ""));
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        FragmentManager fm = getSupportFragmentManager();
+        int count = fm.getBackStackEntryCount();
+        if (count == 1) {
+            finish();
+        } else {
+            toolbar.setVisibility(View.GONE);
+            fm.popBackStack();
+        }
+    }
+
+    private void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void addFragment(Fragment fragment) {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.fragment, fragment)
+                .addToBackStack("LoginFragment")
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+
+    }
+
+    @Override
+    public void onFragmentNav(Fragment fragment, Util.Navigate navigate) {
+
+        switch (navigate) {
+            case FORWARD:
+                break;
+            case BACKWARD:
+                break;
+            case SIGNUP:
+                if (fragment instanceof LoginFragment) {
+                    addFragment(RegistrationFragment.newInstance("", ""));
+                    toolbar.setVisibility(View.VISIBLE);
+                }
+                break;
+            case LOGIN:
+                break;
+            default:
+                break;
+        }
     }
 }
