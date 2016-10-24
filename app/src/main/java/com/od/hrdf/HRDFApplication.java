6232364a22265daa1863b9827b5bd4beb42aae9e
MuhammadMahmoor <mahmoor@opendynamics.com.my>
2016-10-24 10:38:23 +0800

    Relam made as singleton object

--- app/src/main/java/com/od/hrdf/HRDFApplication.java
+++ app/src/main/java/com/od/hrdf/HRDFApplication.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.facebook.drawee.backends.pipeline.Fresco;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -21,6 +22,7 @@ public class HRDFApplication extends Application {
     private RequestQueue requestQueue;
     String newVar;
     public static Context context;
+    public static Realm realm;
 
     @Override
     public void onCreate() {
@@ -28,12 +30,14 @@ public class HRDFApplication extends Application {
         Realm.init(this);
         mInstance = this;
         context = this;
+        Fresco.initialize(this);
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
                 .name(Realm.DEFAULT_REALM_NAME)
                 .schemaVersion(0)
                 .deleteRealmIfMigrationNeeded()
                 .build();
         Realm.setDefaultConfiguration(realmConfiguration);
+        realm = Realm.getInstance(realmConfiguration);
 
         requestQueue = Volley.newRequestQueue(this);
     }
