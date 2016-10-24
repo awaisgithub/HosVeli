509d2eb3eea6b48274d3eaf99ad057f01aec4017
Awais <mmahmoor@gmail.com>
2016-10-24 12:07:38 +0800

    ImageCallback added

---
+++ app/src/main/java/com/od/hrdf/CallBack/ImageCallBack.java
@@ -0,0 +1,12 @@
+package com.od.hrdf.CallBack;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Awais on 10/21/2016.
+ */
+
+public interface ImageCallBack {
+    public void fetchImageSucceed(Bitmap image);
+    public void fetchImageFail(String error);
+}
