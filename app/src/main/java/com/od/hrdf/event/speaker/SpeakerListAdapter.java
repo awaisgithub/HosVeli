cff5285f475741ae0643d233f37a0aefb3512058
MuhammadMahmoor <mahmoor@opendynamics.com.my>
2016-10-27 14:47:49 +0800

    Speakers and Sponsors added

---
+++ app/src/main/java/com/od/hrdf/event/speaker/SpeakerListAdapter.java
@@ -0,0 +1,111 @@
+package com.od.hrdf.event.speaker;
+
+import android.content.Context;
+import android.graphics.drawable.Animatable;
+import android.icu.text.SimpleDateFormat;
+import android.net.ParseException;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.od.hrdf.BOs.Event;
+import com.od.hrdf.BOs.EventSpeaker;
+import com.od.hrdf.BOs.Speaker;
+import com.od.hrdf.HRDFApplication;
+import com.od.hrdf.R;
+import com.od.hrdf.event.EventListAdapterInterface;
+import com.od.hrdf.event.EventListViewHolder;
+
+import java.util.Date;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+
+/**
+ * Created by MuhammadMahmoor on 10/26/16.
+ */
+
+public class SpeakerListAdapter extends RealmRecyclerViewAdapter<EventSpeaker, RecyclerView.ViewHolder> {
+    private Context context;
+    private EventListAdapterInterface eventListAdapterInterface;
+    private OrderedRealmCollection<EventSpeaker> data;
+    private View view;
+
+    public SpeakerListAdapter(@NonNull Context context, @Nullable OrderedRealmCollection<EventSpeaker> data, EventListAdapterInterface eventListAdapterInterface, boolean autoUpdate) {
+        super(context, data, autoUpdate);
+        this.eventListAdapterInterface = eventListAdapterInterface;
+        this.data = data;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        view = LayoutInflater.from(context).inflate(R.layout.adapter_speaker_list, parent, false);
+        return SpeakerListViewHolder.newInstance(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        SpeakerListViewHolder viewHolder = (SpeakerListViewHolder) holder;
+        final EventSpeaker eventSpeaker = data.get(position);
+        Speaker speaker = Speaker.getSpeaker(eventSpeaker.getSpeaker(), HRDFApplication.realm);
+        if(speaker == null)
+            return;
+
+        viewHolder.name.setText(speaker.getName());
+        viewHolder.designation.setText(speaker.getJobTitle());
+        viewHolder.overview.setText(speaker.getOrganization());
+        if(speaker.getImage() != null) {
+            String image = speaker.getImage();
+            image = image.replaceAll(" ", "%20");
+            Uri uri = Uri.parse(image);
+            viewHolder.speakerDrawee.setImageURI(uri);
+            viewHolder.speakerDrawee.setScaleType(ImageView.ScaleType.FIT_XY);
+            DraweeController controller = Fresco.newDraweeControllerBuilder()
+                    .setControllerListener(controllerListener)
+                    .setUri(uri)
+                    .build();
+            viewHolder.speakerDrawee.setController(controller);
+        }
+
+        viewHolder.parent.setTag(speaker.getId());
+        viewHolder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eventListAdapterInterface.gotoDetailActivity(((String)view.getTag()));
+            }
+        });
+    }
+
+    ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+        @Override
+        public void onFinalImageSet(
+                String id,
+                @Nullable ImageInfo imageInfo,
+                @Nullable Animatable anim) {
+
+        }
+
+        @Override
+        public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+
+        }
+
+        @Override
+        public void onFailure(String id, Throwable throwable) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/od/hrdf/event/sponsor/SponsorListFragment.java
@@ -0,0 +1,109 @@
+package com.od.hrdf.event.sponsor;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.od.hrdf.API.Api;
+import com.od.hrdf.BOs.Event;
+import com.od.hrdf.BOs.EventSpeaker;
+import com.od.hrdf.BOs.EventSponsor;
+import com.od.hrdf.BOs.Sponsor;
+import com.od.hrdf.CallBack.FetchCallBack;
+import com.od.hrdf.HRDFApplication;
+import com.od.hrdf.R;
+import com.od.hrdf.Utils.HRDFConstants;
+import com.od.hrdf.event.EventListAdapterInterface;
+import com.od.hrdf.event.speaker.SpeakerListAdapter;
+
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
+public class SponsorListFragment extends Fragment implements EventListAdapterInterface {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    private View rootView;
+    private String eventId;
+
+    public SponsorListFragment() {
+        // Required empty public constructor
+    }
+
+    public static SponsorListFragment newInstance(String param1) {
+        SponsorListFragment fragment = new SponsorListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            eventId = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_sponsor_list, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViews();
+        fetchEventSponsor();
+    }
+
+    private void initViews() {
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.sponsor_rcv);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity()) {
+            @Override
+            protected int getExtraLayoutSpace(RecyclerView.State state) {
+                return 300;
+            }
+        };
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        RealmResults realmResults = null;
+        realmResults = EventSponsor.getDistinctByLevelSeq(HRDFApplication.realm, eventId);
+
+        SponsorListAdapter sponsorListAdapter = new SponsorListAdapter(getActivity(), realmResults, this, true);
+        recyclerView.setAdapter(sponsorListAdapter);
+
+    }
+
+    private void fetchEventSponsor() {
+        RealmQuery query = HRDFApplication.realm.where(EventSponsor.class).equalTo("event", eventId);
+        EventSponsor.fetchEventSponsors(getActivity(), HRDFApplication.realm, Api.urlEventSponsorsList(eventId), query, new FetchCallBack() {
+            @Override
+            public void fetchDidSucceed(RealmResults fetchedItems) {
+                HRDFApplication.realm.beginTransaction();
+                Event.getEvent(eventId, HRDFApplication.realm).setSponsors(EventSponsor.getEventSponsorList(HRDFApplication.realm, eventId));
+                HRDFApplication.realm.commitTransaction();
+            }
+
+            @Override
+            public void fetchDidFail(Exception e) {
+            }
+        });
+    }
+
+    @Override
+    public void gotoDetailActivity(String id) {
+
+    }
+}
