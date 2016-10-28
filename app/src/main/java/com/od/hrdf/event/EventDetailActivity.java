414c05e33e70027ab1e3fc7c994e22450d5073a0
MuhammadMahmoor <mahmoor@opendynamics.com.my>
2016-10-28 18:29:24 +0800

    1- Action layout updated 2- Image scaletype changed

--- app/src/main/java/com/od/hrdf/event/EventDetailActivity.java
+++ app/src/main/java/com/od/hrdf/event/EventDetailActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -23,7 +24,11 @@ import com.facebook.drawee.controller.ControllerListener;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.image.ImageInfo;
+import com.od.hrdf.API.Api;
 import com.od.hrdf.BOs.Event;
+import com.od.hrdf.BOs.User;
+import com.od.hrdf.CallBack.StatusCallBack;
+import com.od.hrdf.Payload.JSONPayloadManager;
 import com.od.hrdf.R;
 import com.od.hrdf.Utils.HRDFConstants;
 import com.od.hrdf.Utils.SectionsPagerAdapter;
@@ -33,6 +38,8 @@ import com.od.hrdf.event.floorplan.FloorPlanFragment;
 import com.od.hrdf.event.speaker.SpeakerListFragment;
 import com.od.hrdf.event.sponsor.SponsorListFragment;
 import com.od.hrdf.landingtab.TabbarActivity;
+
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -91,6 +98,7 @@ public class EventDetailActivity extends AppCompatActivity implements View.OnCli
         findViewById(R.id.event_detail_sponsor).setOnClickListener(this);
         findViewById(R.id.event_detail_exhibitor).setOnClickListener(this);
         findViewById(R.id.event_detail_floorplan).setOnClickListener(this);
+        findViewById(R.id.button_register).setOnClickListener(this);
 
         findViewById(R.id.button_agenda).setOnClickListener(this);
 
@@ -112,6 +120,18 @@ public class EventDetailActivity extends AppCompatActivity implements View.OnCli
 
             }
         });
+
+        String type = getIntent().getStringExtra(HRDFConstants.KEY_EVENT_TYPE);
+        if (type.equalsIgnoreCase(EventFragment.LIST_TYPE_UPCOMING)) {
+            if(event.getBooked()) {
+                ((TextView)findViewById(R.id.button_register)).setText("Registered");
+            } else {
+
+            }
+        } else if (type.equalsIgnoreCase(EventFragment.LIST_TYPE_ARCHIVE)) {
+            findViewById(R.id.layout_actions).setVisibility(View.GONE);
+            findViewById(R.id.button_register).setVisibility(View.GONE);
+        }
     }
 
     private void setDateTimeView(TextView tv) {
@@ -170,9 +190,41 @@ public class EventDetailActivity extends AppCompatActivity implements View.OnCli
                 intent.putExtra(HRDFConstants.KEY_EVENT_ID, event.getId());
                 startActivity(intent);
                 break;
+            case R.id.button_register:
+                if(!event.getBooked())
+                    bookEvent();
+                break;
             default:
                 break;
         }
+    }
+
+    private void bookEvent() {
+        final User user = User.getCurrentUser(realm);
+        Event.bookEvent(user.getId(), event.getId(), Api.urlJogetCRUD(), new StatusCallBack() {
+            @Override
+            public void success(JSONObject response) {
+                Log.i(HRDFConstants.TAG, "bookEvent success=" + response);
+                if (response.has("status")) {
+                    String status = response.optString("status");
+                    if (status.equalsIgnoreCase("1")) {
+                        ((TextView)findViewById(R.id.button_register)).setText("Registered");
+                        realm.beginTransaction();
+                        event.setBooked(true);
+                        event.setUserId(user.getId());
+                        user.getEvents().add(event);
+                        realm.commitTransaction();
+                    } else {
+                    }
+                } else {
+                }
+            }
+
+            @Override
+            public void failure(String response) {
+                Log.i(HRDFConstants.TAG, "bookEvent failure=" + response);
+            }
+        });
     }
 
     ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
