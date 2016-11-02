c2f77947c01f042e875ac9c777549a8c4190a64f
MuhammadMahmoor <mahmoor@opendynamics.com.my>
2016-11-02 14:17:57 +0800

    1- Interface added for agenda 2- Util added to make CapWords the sentence 3- New background drawable added for exhibitor detail card 4- Placeholder added

---
+++ app/src/main/java/com/od/hrdf/event/agenda/AgendaFragActivityNotifier.java
@@ -0,0 +1,10 @@
+package com.od.hrdf.event.agenda;
+
+/**
+ * Created by MuhammadMahmoor on 11/2/16.
+ */
+
+public interface AgendaFragActivityNotifier {
+    public void noAgendaAvailable();
+    public void agendaAvailable();
+}
